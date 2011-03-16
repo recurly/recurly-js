2be612bc24d71aa302908d546674f643c63a1cc9
Robert Righter <robrighter@gmail.com>
2011-03-16 18:03:53 -0400

    completed wrappers for accounts and billing

--- example.js
+++ example.js
@@ -1,11 +1,12 @@
-
-
+var sys = require('sys');
 var Recurly = require('./lib/recurly');
-
-
+var callback = function(result){
+  console.log(sys.inspect(result));
+};
 
 recurly = new Recurly(require('./config'));
-
-recurly.request('/accounts.json', 'GET', function(result){
-  console.log(result);
-});
+//recurly.accounts.updateAccount('123',{email: 'me8@there.com'},callback);
+//recurly.accounts.closeAccount('4566',callback);
+//recurly.accounts.listAccounts(callback,'active_subscribers');
+//recurly.accounts.getAccount('robrighter@gmail.com',callback);
+recurly.billingInfo.deleteBillingInfo('robrighter@gmail.com',callback);
