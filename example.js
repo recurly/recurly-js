2c888d6a146fbf8bb83eed7267857137f52394e8
Robert Righter <robrighter@gmail.com>
2011-03-17 17:37:23 -0400

    Charges, Coupons, Credits, invoices, subscription-plans

--- example.js
+++ example.js
@@ -5,8 +5,32 @@ var callback = function(result){
 };
 
 recurly = new Recurly(require('./config'));
-//recurly.accounts.updateAccount('123',{email: 'me8@there.com'},callback);
-//recurly.accounts.closeAccount('4566',callback);
-//recurly.accounts.listAccounts(callback,'active_subscribers');
-//recurly.accounts.getAccount('robrighter@gmail.com',callback);
-recurly.billingInfo.deleteBillingInfo('robrighter@gmail.com',callback);
+// recurly.charges.chargeAccount('robrighter@gmail.com',{
+//   amount_in_cents: '850',
+//   description: 'testing the charge'
+// },callback);
+
+//recurly.charges.listAll('robrighter@gmail.com',callback);
+
+// recurly.coupons.redeemOnAccount('robrighter@gmail.com', {
+//    coupon_code: '50PERCENTOFF'
+// },callback);
+
+//recurly.coupons.removeFromAccount('robrighter@gmail.com',callback);
+
+//recurly.coupons.getAssociatedWithAccount('robrighter@gmail.com',callback);
+
+// recurly.credits.creditAccount('robrighter@gmail.com',{
+//   amount_in_cents: 550,
+//   description: 'Cutting you a break 3'
+// }, callback);
+
+//recurly.credits.listAll('robrighter@gmail.com',callback);
+
+//recurly.invoices.getAssociatedWithAccount('robrighter@gmail.com',callback);
+//recurly.invoices.get('7aba9e26feae42c1acb078fea1024c6f',callback);
+//recurly.invoices.invoiceAccount('robrighter@gmail.com',callback);
+
+//recurly.subscriptions.getAssociatedWithAccount('robrighter@gmail.com',callback);
+
+recurly.subscriptionPlans.listAll(callback);
