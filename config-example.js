fcaf63f1249d4c90adbe1de71b08ce2d7dfdda15
Iván Guardado <contact@ivanguardado.com>
2012-09-30 23:41:04 +0200

    Updated to API v2

--- config-example.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	API_USERNAME: '',
-	API_PASSWORD: '',
-	PRIVATE_KEY:  '',
-	SUBDOMAIN:    '',
-	ENVIRONMENT:  'sandbox',
-	DEBUG: false
-};
---
+++ test/config-example.js
@@ -0,0 +1,9 @@
+module.exports = {
+	API_USERNAME: '',
+	API_PASSWORD: '',
+	PRIVATE_KEY:  '',
+	SUBDOMAIN:    '',
+	ENVIRONMENT:  'sandbox',
+	DEBUG: true,
+	API_VERSION: 2
+};
