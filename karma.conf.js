ffe278442ab58f982ad19e7a29851eccf3a0c213
Christopher Rogers <chrissrogers@gmail.com>
2015-12-22 01:23:26 -0600

    Moves HostedFields init concerns to the bus
    - Adapts bus to emit bus:added event when an emitter object is added
    - Adds recurly.ready method
    - documents recurly.readyState
    
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
-    singleRun: false,
+    singleRun: true,
     concurrency: Infinity
   });
 };
