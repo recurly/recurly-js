329520969460e03a8460ec3f5e83b4d018688ff0
Jim Snodgrass <snodgrass23@gmail.com>
2016-04-20 10:13:00 -0400

    rename a few things into a fraud namespace

--- test/fraud.test.js
+++ test/fraud.test.js
@@ -1,18 +1,20 @@
 import assert from 'assert';
 import clone from 'component-clone';
 import {Recurly} from '../lib/recurly';
-import FraudDataCollector from '../lib/recurly/fraud-data-collector';
+import Fraud from '../lib/recurly/fraud';
 import {fixture} from './support/fixtures';
 
 const defaultConfig = {
   publicKey: 'test',
-  fraudDataCollector: true,
-  litleSessionId: '98as6d09df907asd'
+  fraud: {
+    dataCollector: true,
+    litleSessionId: '98as6d09df907asd'
+  }
 };
 var recurly;
 var config;
 
-describe('FraudDataCollector', () => {
+describe('Fraud', () => {
   describe('params', () => {
     var mockRecurlyInstance;
     var fraud_session_id = 'a0s89d09adfs';
@@ -23,39 +25,39 @@ describe('FraudDataCollector', () => {
     });
 
     it('inserts both fraud processor session ids when configured', () => {
-      let fraudParams = FraudDataCollector.params.call(mockRecurlyInstance, inputs);
+      let fraudParams = Fraud.params.call(mockRecurlyInstance, inputs);
       assert(fraudParams.length == 2);
       assert(fraudParams[0].processor == 'kount');
       assert(fraudParams[0].session_id == fraud_session_id);
       assert(fraudParams[1].processor == 'litle_threat_metrix');
-      assert(fraudParams[1].session_id == defaultConfig.litleSessionId);
+      assert(fraudParams[1].session_id == defaultConfig.fraud.litleSessionId);
     });
 
     it('inserts only kount processor when litle not configured', () => {
-      mockRecurlyInstance.config.litleSessionId = null;
-      let fraudParams = FraudDataCollector.params.call(mockRecurlyInstance, inputs);
+      mockRecurlyInstance.config.fraud.litleSessionId = null;
+      let fraudParams = Fraud.params.call(mockRecurlyInstance, inputs);
       assert(fraudParams.length == 1);
       assert(fraudParams[0].processor == 'kount');
       assert(fraudParams[0].session_id == fraud_session_id);
     });
 
     it('inserts only litle processor when only litle and not kount configured', () => {
-      mockRecurlyInstance.config.fraudDataCollector = false;
-      let fraudParams = FraudDataCollector.params.call(mockRecurlyInstance, inputs);
+      mockRecurlyInstance.config.fraud.dataCollector = false;
+      let fraudParams = Fraud.params.call(mockRecurlyInstance, inputs);
       assert(fraudParams.length == 1);
       assert(fraudParams[0].processor == 'litle_threat_metrix');
-      assert(fraudParams[0].session_id == defaultConfig.litleSessionId);
+      assert(fraudParams[0].session_id == defaultConfig.fraud.litleSessionId);
     });
 
     it('returns empty array when both processors are not configured or ran', () => {
-      mockRecurlyInstance.config.litleSessionId = null;
-      mockRecurlyInstance.config.fraudDataCollector = false;
-      let fraudParams = FraudDataCollector.params.call(mockRecurlyInstance, inputs);
+      mockRecurlyInstance.config.fraud.litleSessionId = null;
+      mockRecurlyInstance.config.fraud.dataCollector = false;
+      let fraudParams = Fraud.params.call(mockRecurlyInstance, inputs);
       assert(fraudParams.length == 0);
     });
   });
 
-  describe('addFields', () => {
+  describe('dataCollector', () => {
 
 
     beforeEach(() => {
@@ -68,7 +70,7 @@ describe('FraudDataCollector', () => {
     });
 
     it("doesn't run unless set to true in config", () => {
-      config.fraudDataCollector = false;
+      config.fraud.dataCollector = false;
       initializeRecurlyWith(mockedRequest("some kind of server error", null));
       assert(!recurly.request.calledOnce);
     });
