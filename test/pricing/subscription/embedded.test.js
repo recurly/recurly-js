84e44fe639b1b598628fc8bd79124cf8fe78e688
Christopher Rogers <chrissrogers@gmail.com>
2017-11-09 11:25:34 -0800

    Fixes EmbeddedSubscriptionPricing specs
    - `name` was unreliable. Spies are.
    
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- test/pricing/subscription/embedded.test.js
+++ test/pricing/subscription/embedded.test.js
@@ -8,6 +8,14 @@ describe('EmbeddedSubscriptionPricing', () => {
     this.recurly = initRecurly();
     this.subscriptionPricingDefault = this.recurly.Pricing.Subscription();
     this.subscriptionPricing = this.recurly.Pricing.Subscription();
+
+    // Spies
+    this.originalMethods = {};
+    DISABLED_METHODS.forEach(method => {
+      sinon.stub(this.subscriptionPricing, method);
+      this.originalMethods[method] = this.subscriptionPricing[method];
+    });
+
     this.embeddedSubscriptionPricing = new EmbeddedSubscriptionPricing(this.subscriptionPricing);
   });
 
@@ -16,12 +24,25 @@ describe('EmbeddedSubscriptionPricing', () => {
       DISABLED_METHODS.forEach(method => {
         assert.equal(this.subscriptionPricingDefault[method], SubscriptionPricing.prototype[method]);
         assert.notEqual(this.subscriptionPricing[method], SubscriptionPricing.prototype[method]);
+
+        // Calling the method does not invoke its original form
+        this.subscriptionPricing[method]();
+        assert(this.originalMethods[method].notCalled);
       });
     });
 
     it('proxies the disabled methods onto the EmbeddedSubscriptionPricing instance', function () {
       DISABLED_METHODS.forEach(method => {
-        assert.equal(this.embeddedSubscriptionPricing[method].name, SubscriptionPricing.prototype[method].name);
+        assert.equal(this.subscriptionPricingDefault[method].prototype, SubscriptionPricing.prototype[method].prototype);
+
+        // Loose assertion that this is now a bound method
+        assert.equal(typeof this.embeddedSubscriptionPricing[method], 'function');
+        assert.equal(this.embeddedSubscriptionPricing[method].prototype, undefined);
+
+        // Calling the method does invoke its original
+        assert(this.originalMethods[method].notCalled);
+        this.embeddedSubscriptionPricing[method]();
+        assert(this.originalMethods[method].calledOnce);
       });
     });
   });
