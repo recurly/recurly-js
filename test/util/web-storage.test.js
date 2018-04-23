26269bbeab12dd6a7b97d1bfc44a15aaa3f848f1
Christopher Rogers <chrissrogers@gmail.com>
2018-04-23 16:02:03 -0600

    Allows deviceId and sessionId to be set directly
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- test/util/web-storage.test.js
+++ test/util/web-storage.test.js
@@ -1,10 +1,11 @@
 import assert from 'assert';
-import {storage, NAMESPACE} from '../../lib/util/web-storage';
+import {fetch, set, NAMESPACE} from '../../lib/util/web-storage';
 
 describe('storage', () => {
   const localStorage = window.localStorage;
   const sessionStorage = window.sessionStorage;
   const example = 'example';
+  const exampleKey = `${NAMESPACE}.test`;
 
   beforeEach(() => {
     localStorage.clear();
@@ -18,28 +19,50 @@ describe('storage', () => {
     sessionStorage.clear();
   });
 
-  it('fetches from localStorage by default', () => {
-    assert.strictEqual(storage({ key: `${NAMESPACE}.test` }), null);
-    localStorage.setItem(`${NAMESPACE}.test`, example);
-    assert.strictEqual(storage({ key: 'test' }), example);
+
+  describe('fetch', () => {
+    it('fetches from localStorage by default', () => {
+      assert.strictEqual(fetch({ key: exampleKey }), null);
+      localStorage.setItem(exampleKey, example);
+      assert.strictEqual(fetch({ key: 'test' }), example);
+    });
+
+    it('fetches from sessionStorage', () => {
+      assert.strictEqual(fetch({ scope: 'session', key: exampleKey }), null);
+      sessionStorage.setItem(exampleKey, example);
+      assert.strictEqual(fetch({ scope: 'session', key: 'test' }), example);
+    });
+
+    it('sets the value if given `otherwise` and the key does not exist', () => {
+      assert.strictEqual(fetch({ key: 'test' }), null);
+      fetch({ key: 'test', otherwise: example });
+      assert.strictEqual(fetch({ key: 'test' }), example);
+      assert.strictEqual(localStorage.getItem(exampleKey), example);
+    });
+
+    it('fetches a previously-set value', () => {
+      assert.strictEqual(fetch({ key: 'test' }), null);
+      assert.strictEqual(fetch({ key: 'test', otherwise: example }), example);
+      assert.strictEqual(fetch({ key: 'test' }), example);
+    });
   });
 
-  it('fetches from sessionStorage', () => {
-    assert.strictEqual(storage({ scope: 'session', key: `${NAMESPACE}.test` }), null);
-    sessionStorage.setItem(`${NAMESPACE}.test`, example);
-    assert.strictEqual(storage({ scope: 'session', key: 'test' }), example);
-  });
+  describe('set', () => {
+    it('sets on localStorage by default', () => {
+      assert.strictEqual(localStorage.getItem(exampleKey), null);
+      set({ key: 'test', value: example });
+      assert.strictEqual(localStorage.getItem(exampleKey), example);
+    });
 
-  it('sets the value if given `otherwise` and the key does not exist', () => {
-    assert.strictEqual(storage({ key: 'test' }), null);
-    storage({ key: 'test', otherwise: example });
-    assert.strictEqual(storage({ key: 'test' }), example);
-    assert.strictEqual(localStorage.getItem(`${NAMESPACE}.test`), example);
-  });
+    it('sets on sessionStorage when specified', () => {
+      assert.strictEqual(sessionStorage.getItem(exampleKey), null);
+      set({ scope: 'session', key: 'test', value: example });
+      assert.strictEqual(sessionStorage.getItem(exampleKey), example);
+    });
 
-  it('fetches a previously-set value', () => {
-    assert.strictEqual(storage({ key: 'test' }), null);
-    assert.strictEqual(storage({ key: 'test', otherwise: example }), example);
-    assert.strictEqual(storage({ key: 'test' }), example);
+    it('returns the new value', () => {
+      assert.strictEqual(localStorage.getItem(exampleKey), null);
+      assert.strictEqual(set({ key: 'test', value: example }), example);
+    });
   });
 });
