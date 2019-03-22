6fea3473a4490b67563da489996c80471e6a3d0f
Christopher Rogers <chrissrogers@gmail.com>
2019-03-22 08:27:08 -0700

    Adds Bus#remove - Allows a recipient to be cleanly removed from a Bus instance

--- test/bus.test.js
+++ test/bus.test.js
@@ -1,11 +1,18 @@
 import assert from 'assert';
+import isEqual from 'lodash.isequal';
 import {applyFixtures} from './support/fixtures';
 import {Bus} from '../lib/recurly/bus';
 import Emitter from 'component-emitter';
 
-describe('Recurly.Bus', function () {
+class EmitterInheritant extends Emitter {}
+
+describe.only('Recurly.Bus', function () {
   beforeEach(function () {
     this.bus = new Bus({ api: `//${window.location.host}/api` });
+    this.exampleEmitters = [
+      (new Emitter),
+      (new EmitterInheritant)
+    ];
   });
 
   afterEach(function () {
@@ -14,13 +21,6 @@ describe('Recurly.Bus', function () {
 
   describe('Bus.add', () => {
     describe('when adding an Emitter recipient', function () {
-      beforeEach(function () {
-        this.exampleEmitters = [
-          (new Emitter),
-          (new EmitterInheritant)
-        ];
-      });
-
       it('adds the Emitters to the recipients', function () {
         this.exampleEmitters.forEach(exampleEmitter => {
           this.bus.add(exampleEmitter);
@@ -54,6 +54,34 @@ describe('Recurly.Bus', function () {
       });
     });
   });
-});
 
-class EmitterInheritant extends Emitter {}
+  describe('Bus.remove', () => {
+    describe('when recipients exist on the bus', () => {
+      beforeEach(function () {
+        this.exampleEmitters.forEach(e => this.bus.add(e));
+      });
+
+
+      it('removes a recipient and returns true when the recipient had been added', function () {
+        assert(isEqual(this.bus.recipients, this.exampleEmitters));
+        assert.strictEqual(this.bus.remove(this.exampleEmitters[0]), true);
+        assert(isEqual(this.bus.recipients, [this.exampleEmitters[1]]));
+      });
+
+      it('does not remove a recipient and returns false when the recipient had not been added', function () {
+        const foreignRecipient = new Emitter;
+        assert(isEqual(this.bus.recipients, this.exampleEmitters));
+        assert.strictEqual(this.bus.remove(foreignRecipient), false);
+        assert(isEqual(this.bus.recipients, this.exampleEmitters));
+      });
+    });
+
+    describe('when no recipients exist on the bus', () => {
+      it('does not change the recipients and returns false', function () {
+        assert.strictEqual(this.bus.recipients.length, 0);
+        assert.strictEqual(this.bus.remove(this.exampleEmitters[0]), false);
+        assert.strictEqual(this.bus.recipients.length, 0);
+      });
+    });
+  });
+});
