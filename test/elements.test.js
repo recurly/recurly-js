578e85bf75b9b1a33acd9f2fd5d435ccfc4267b4
Christopher Rogers <chrissrogers@gmail.com>
2019-04-04 11:10:53 -0700

    Adds Elements.onElementAttach and Elements.onElementSubmit tests

--- test/elements.test.js
+++ test/elements.test.js
@@ -16,8 +16,9 @@ describe('Elements', function () {
     this.elements = new Elements({ recurly });
 
     // This stub allows us to generate inert Element instances
-    this.elementsStub = new ElementsStub({ recurly })
+    this.elementsStub = new ElementsStub({ recurly });
     this.cardElementExample = this.elementsStub.CardElement();
+    this.cardElementExampleTwo = this.elementsStub.CardElement();
   });
 
   it('has factory properties that return Element instances', function () {
@@ -38,6 +39,7 @@ describe('Elements', function () {
 
   describe('Elements.add', function () {
     it('only accepts an Element', function () {
+      const { elements, cardElementExample } = this;
       const invalidExamples = [
         undefined,
         null,
@@ -49,33 +51,36 @@ describe('Elements', function () {
       ];
 
       invalidExamples.forEach(example => {
-        assert.throws(() => this.elements.add(example), /Invalid element\./);
+        assert.throws(() => elements.add(example), /Invalid element\./);
       });
 
       // This should not error
-      this.elements.add(this.cardElementExample);
+      elements.add(cardElementExample);
     });
 
     it('allows only one of a single Element type to be added', function () {
-      const cardElementExampleTwo = this.elementsStub.CardElement();
+      const { elements, cardElementExample } = this;
 
-      this.elements.add(this.cardElementExample);
+      elements.add(cardElementExample);
 
       assert.throws(() => {
-        this.elements.add(cardElementExampleTwo);
+        elements.add(cardElementExampleTwo);
       }, 'Invalid element. There is already a `CardElement` in this set.');
     });
 
     it('adds the Element to the set', function () {
-      this.elements.add(this.cardElementExample);
-      assert.strictEqual(!!~this.elements.elements.indexOf(this.cardElementExample), true);
-    })
+      const { elements, cardElementExample } = this;
+      elements.add(cardElementExample);
+      assert.strictEqual(!!~elements.elements.indexOf(cardElementExample), true);
+    });
 
     it('enforces valid Element set rules', function () {
+      const { recurly, elementsStub } = this;
+
       Elements.VALID_SETS.forEach(validSet => {
-        const elements = new Elements({ recurly: this.recurly });
+        const elements = new Elements({ recurly });
         validSet.forEach(elementName => {
-          const element = this.elementsStub[elementName]();
+          const element = elementsStub[elementName]();
           elements.add(element);
           assert.strictEqual(!!~elements.elements.indexOf(element), true);
         });
@@ -88,10 +93,10 @@ describe('Elements', function () {
         ['NumberElement', 'MonthElement', 'YearElement', 'CvvElement', 'CardElement']
       ];
       invalidSets.forEach(invalidSet => {
-        const elements = new Elements({ recurly: this.recurly });
+        const elements = new Elements({ recurly: recurly });
         assert.throws(() => {
           invalidSet.forEach(elementName => {
-            const element = this.elementsStub[elementName]();
+            const element = elementsStub[elementName]();
             elements.add(element);
           });
         }, /Invalid element\. A `(\w+)` may not be added to the existing set/);
@@ -101,43 +106,207 @@ describe('Elements', function () {
 
   describe('Elements.remove', function () {
     beforeEach(function () {
-      this.elements.add(this.cardElementExample);
+      const { elements, cardElementExample } = this;
+      elements.add(cardElementExample);
     });
 
     it('removes the Element', function () {
-      assert.strictEqual(this.elements.elements.length, 1);
-      assert.strictEqual(!!~this.elements.elements.indexOf(this.cardElementExample), true);
-      this.elements.remove(this.cardElementExample);
-      assert.strictEqual(this.elements.elements.length, 0);
-      assert.strictEqual(!!~this.elements.elements.indexOf(this.cardElementExample), false);
+      const { elements, cardElementExample } = this;
+      assert.strictEqual(elements.elements.length, 1);
+      assert.strictEqual(!!~elements.elements.indexOf(cardElementExample), true);
+      elements.remove(cardElementExample);
+      assert.strictEqual(elements.elements.length, 0);
+      assert.strictEqual(!!~elements.elements.indexOf(cardElementExample), false);
     });
   });
 
   describe('Elements.destroy', function () {
     beforeEach(function () {
-      this.elements.add(this.cardElementExample);
+      const { elements, cardElementExample } = this;
+      elements.add(cardElementExample);
     });
 
     it('calls destroy on its Element instances', function () {
-      sinon.spy(this.cardElementExample, 'destroy');
-      assert.strictEqual(this.cardElementExample.destroy.called, false);
-      this.elements.destroy();
-      assert.strictEqual(this.cardElementExample.destroy.calledOnce, true);
+      const { elements, cardElementExample } = this;
+      sinon.spy(cardElementExample, 'destroy');
+      assert.strictEqual(cardElementExample.destroy.called, false);
+      elements.destroy();
+      assert.strictEqual(cardElementExample.destroy.calledOnce, true);
     });
 
     it('destroys its bus instance', function () {
-      sinon.spy(this.elements.bus, 'destroy');
-      assert.strictEqual(this.elements.bus.destroy.called, false);
-      this.elements.destroy();
-      assert.strictEqual(this.elements.bus.destroy.calledOnce, true);
+      const { elements, cardElementExample } = this;
+      const { bus } = elements
+      sinon.spy(bus, 'destroy');
+      assert.strictEqual(bus.destroy.called, false);
+      elements.destroy();
+      assert.strictEqual(bus.destroy.calledOnce, true);
     });
   });
 
-  describe(`Event: 'submit'`, function () {
-    it('fires when an Element emits the submit event', function (done) {
-      this.elements.on('submit', () => done());
-      this.elements.add(this.cardElementExample);
-      this.cardElementExample.emit('submit')
+  describe('listeners', function () {
+    describe('onElementAttach', function () {
+      const stubGetter = (prop, val) => el => sinon.stub(el, prop).get(() => val);
+
+      beforeEach(function () {
+        sinon.spy(this.elements.bus, 'send');
+      });
+
+      afterEach(function () {
+        this.elements.bus.send.restore();
+      });
+
+      it(`is called when an Element emits the 'attach' event`, function () {
+        const { elements, cardElementExample } = this;
+        sinon.spy(elements, 'onElementAttach');
+        elements.add(cardElementExample);
+        cardElementExample.emit('attach');
+        assert(elements.onElementAttach.calledOnce);
+        elements.onElementAttach.restore();
+      });
+
+      describe('when no elements are added', function () {
+        it(...doesNotSendElementsReadyMessage());
+      });
+
+      describe('when one element is added', function () {
+        beforeEach(function () {
+          const { elements } = this;
+          this.card = elements.CardElement();
+        });
+
+        describe('when it has begun attachment', function () {
+          beforeEach(function () {
+            stubGetter('attaching', true)(this.card);
+          });
+
+          describe('when it is not yet attached', function () {
+            it(...doesNotSendElementsReadyMessage());
+          });
+
+          describe('when it is attached', function () {
+            beforeEach(function () {
+              stubGetter('attaching', false)(this.card);
+              stubGetter('attached', true)(this.card);
+            });
+
+            it(...sendsElementsReadyMessage());
+          });
+        });
+      });
+
+      // TODO: run this suite for the previous scenario
+      describe('when multiple elements are added', function () {
+        beforeEach(function () {
+          const { elements } = this;
+          this.number = elements.NumberElement();
+          this.month = elements.MonthElement();
+          this.year = elements.YearElement();
+          this.cvv = elements.CvvElement();
+        });
+
+        describe('when some elements have begun attachment', function () {
+          beforeEach(function () {
+            const { number, month, year } = this;
+            [number, month, year].forEach(stubGetter('attaching', true));
+          });
+
+          describe('when none of those elements are yet attached', function () {
+            it(...doesNotSendElementsReadyMessage());
+          });
+
+          describe('when some of those elements are attached', function () {
+            beforeEach(function () {
+              const { number, month } = this;
+              [number, month].forEach(stubGetter('attaching', false));
+              [number, month].forEach(stubGetter('attached', true));
+            });
+
+            it(...doesNotSendElementsReadyMessage());
+          });
+
+          describe('when all of those elements are attached', function () {
+            beforeEach(function () {
+              const { number, month, year } = this;
+              [number, month, year].forEach(stubGetter('attaching', false));
+              [number, month, year].forEach(stubGetter('attached', true));
+            });
+
+            it(...sendsElementsReadyMessage());
+          });
+        });
+
+        describe('when all elements have begun attachment', function () {
+          beforeEach(function () {
+            const { number, month, year, cvv } = this;
+            [number, month, year, cvv].forEach(stubGetter('attaching', true));
+          });
+
+          describe('when none of those elements are yet attached', function () {
+            it(...doesNotSendElementsReadyMessage());
+          });
+
+          describe('when some of those elements are attached', function () {
+            beforeEach(function () {
+              const { number, month, year } = this;
+              [number, month, year].forEach(stubGetter('attaching', false));
+              [number, month, year].forEach(stubGetter('attached', true));
+            });
+
+            it(...doesNotSendElementsReadyMessage());
+          });
+
+          describe('when all of those elements are attached', function () {
+            beforeEach(function () {
+              const { number, month, year, cvv } = this;
+              [number, month, year, cvv].forEach(stubGetter('attaching', false));
+              [number, month, year, cvv].forEach(stubGetter('attached', true));
+            });
+
+            it(...sendsElementsReadyMessage());
+          });
+        });
+      });
+
+      function doesNotSendElementsReadyMessage () {
+        return [`does not send the 'elements:ready!' message`, function () {
+          const { elements } = this;
+          elements.onElementAttach();
+          assert(elements.bus.send.neverCalledWith('elements:ready!'));
+        }];
+      }
+
+      function sendsElementsReadyMessage () {
+        return [`sends the 'elements:ready!' message`, function () {
+          const { elements } = this;
+          const { bus } = elements;
+          elements.onElementAttach();
+          assert(bus.send.calledOnceWithExactly('elements:ready!'));
+        }];
+      }
+    });
+
+    describe('onElementSubmit', function () {
+      it(`is called when an Element emits the 'submit' event,
+          passing along the Element responsible`, function () {
+        const { elements, cardElementExample } = this;
+        sinon.spy(elements, 'onElementSubmit');
+        elements.add(cardElementExample);
+        cardElementExample.onSubmit();
+        assert(elements.onElementSubmit.calledOnceWithExactly(cardElementExample));
+        elements.onElementSubmit.restore();
+      });
+
+      it(`emits the 'submit' event`, function (done) {
+        const { elements } = this;
+        sinon.spy(elements, 'emit');
+        elements.on('submit', () => {
+          assert(elements.emit.calledOnceWithExactly('submit', 'test'));
+          elements.emit.restore();
+          done();
+        });
+        elements.onElementSubmit('test');
+      });
     });
   });
 });
