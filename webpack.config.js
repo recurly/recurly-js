94600c9747cde47a66002d1f37cad62a59544367
Christopher Rogers <chrissrogers@gmail.com>
2019-04-29 04:27:45 -0700

    Adds browser info to token creation - Moves browserInfo from ThreeDSecureConcern to Risk - Applies browserInfo to all token requests

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
               cacheDirectory: true,
               plugins: [
                 '@babel/plugin-transform-object-assign',
-                '@babel/plugin-proposal-class-properties'
+                '@babel/plugin-proposal-class-properties',
+                '@babel/plugin-proposal-export-default-from'
               ],
               presets: [
                 ['@babel/preset-env', {
