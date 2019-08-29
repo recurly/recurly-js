650b7529271c517efdcdf3651f60b687d2ad8c68
Dave Brudner <dbrudne1@gmail.com>
2019-08-29 15:52:54 -0500

    adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parser': 'babel-eslint',
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'space-before-function-paren': [
+            'error',
+            'always'
+        ],
+        'object-curly-spacing': [
+            'error',
+            'always'
+        ],
+        'eol-last': [
+            'error',
+            'always'
+        ]
+    }
+};
