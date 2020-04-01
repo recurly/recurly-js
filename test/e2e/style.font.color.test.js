919685d79aaf0754f3ae162da9a60d70b646b29e
Alex Tom <atom@recurly.com>
2020-03-31 17:39:10 -0700

    Added invalid : fontColor test case

--- test/e2e/style.font.color.test.js
+++ test/e2e/style.font.color.test.js
@@ -56,7 +56,7 @@ describe('Expiration date validations', async () => {
    });
 
   // The font color should changed to red if an incorrect data was entered
-  it('Test expiry changed font color to red', async function () {
+  it('Test card fields should changed font color to default red', async function () {
     await browser.switchToFrame(0);
     const number = await $(sel.number)
     const expiry = await $(sel.expiry)
@@ -86,4 +86,32 @@ describe('Expiration date validations', async () => {
     await assertStyleColorIs(expiry, '#e35256');
     await assertStyleColorIs(cvv, '#e35256');
   });
+
+  // After set the invalid fontColor to yellow, the fontColor should not be the default red
+  it('Test card fields should not changed font color to default red', async function () {
+    // Change the default invalid fontColor
+    await browser.switchToFrame(null);
+    const result = await styleHostedField(FIELD_TYPES.CARD, { invalid: {fontColor: 'yellow'} });
+
+    await browser.switchToFrame(0);
+    const number = await $(sel.number)
+    const expiry = await $(sel.expiry)
+    const cvv = await $(sel.cvv)
+    
+    //Now enter all invalid entries
+    await number.setValue('4111 1111 111A 1111')
+    await expiry.setValue('124')
+    await cvv.setValue('1X3')
+
+    //Switch set the focus back to the main frame
+    await browser.switchToFrame(null);
+    await (await $(sel.firstName)).addValue('');
+
+    //Switch back to the iframe and the card fields font color should all be yellow
+    await browser.switchToFrame(0);
+    await assertStyleColorIs(number, '#ffff00');
+    await assertStyleColorIs(expiry, '#ffff00');
+    await assertStyleColorIs(cvv, '#ffff00');
+  });
+
 });
