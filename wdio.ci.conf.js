d1e3974a5ec12b554cb2400806b4b20da48d2ce5
Dave Brudner <dbrudne1@gmail.com>
2020-06-03 15:59:07 -0500

    Adds e2e tests for electron

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -17,41 +17,45 @@ const { timeout } = defaultConfig.mochaOpts;
 
 spawnSync('mkdir', ['-p', 'build/reports/e2e/log'] );
 
-const config = exports.config = Object.assign({}, defaultConfig, {
-  user,
-  key,
-  browserstackLocal: true,
-  browserstackOpts: {
-    logfile: 'build/reports/e2e/log/browserstack.log',
-    localIdentifier
-  },
-  capabilities: [
-    {
-      'bstack:options': Object.assign(
-        {},
-        capabilities[`bs_${BROWSER || 'chrome'}`],
-        {
-          projectName,
-          buildName: `${TRAVIS_BUILD_NUMBER || `Local e2e [${branchName()}]`}`,
-          seleniumVersion: '3.141.59',
-          appiumVersion: '1.16.0',
-          local: true,
-          debug: true,
-          networkLogs: true,
-          consoleLogs: 'verbose',
-          localIdentifier
-        }
-      ),
-      'browserstack.use_w3c': true,
-      captureTimeout: timeout,
-      pollingTimeout: timeout,
-      timeout
-    }
-  ],
-  baseUrl: 'http://bs-local.com:9877',
-  services: [
-    ['browserstack']
-  ]
-});
+if (BROWSER === 'electron') {
+  exports.config = defaultConfig;
+} else {
+  const config = exports.config = Object.assign({}, defaultConfig, {
+    user,
+    key,
+    browserstackLocal: true,
+    browserstackOpts: {
+      logfile: 'build/reports/e2e/log/browserstack.log',
+      localIdentifier
+    },
+    capabilities: [
+      {
+        'bstack:options': Object.assign(
+          {},
+          capabilities[`bs_${BROWSER || 'chrome'}`],
+          {
+            projectName,
+            buildName: `${TRAVIS_BUILD_NUMBER || `Local e2e [${branchName()}]`}`,
+            seleniumVersion: '3.141.59',
+            appiumVersion: '1.16.0',
+            local: true,
+            debug: true,
+            networkLogs: true,
+            consoleLogs: 'verbose',
+            localIdentifier
+          }
+        ),
+        'browserstack.use_w3c': true,
+        captureTimeout: timeout,
+        pollingTimeout: timeout,
+        timeout
+      }
+    ],
+    baseUrl: 'http://bs-local.com:9877',
+    services: [
+      ['browserstack']
+    ]
+  });
 
-delete config.path;
+  delete config.path;
+}
