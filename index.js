a8952f6c000e2d6a9ad28bf702deebc0ee8ab8e1
Christopher Rogers <chrissrogers@gmail.com>
2020-07-16 12:00:24 -0700

    Adds open source attribution comment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * License information available at https://docs.recurly.com/page/open-source-attribution
+ * @attribution
+ */
+
 import recurlyCSS from './lib/recurly.css';
 import { Recurly } from './lib/recurly';
 
