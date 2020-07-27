b210c119a79dfae1eb27aa6696e2bb9f83d29cea
Joe Gutierrez <jgutierrez@recurly.com>
2020-07-27 16:20:47 -0600

    Add negative unit test

--- test/server/fixtures/token/index.js
+++ test/server/fixtures/token/index.js
@@ -5,11 +5,13 @@
 var DECLINE_CARD = '4000000000000002';
 var INVALID_JSON = '5454545454545454';
 var DECLINED_MESSAGE = 'Your card was declined. In order to resolve the issue, you will need to contact your bank.';
+var DECLINED_TAX_IDENTIFIER_TYPE = 'Tax identifier type must be one of the following: ["cpf"]';
 
 module.exports = function token () {
   var params = this.method === 'GET' ? this.query : this.request.body;
   if (params.number === DECLINE_CARD) return decline;
   if (params.number === INVALID_JSON) return invalidJson;
+  if (params.tax_identifier_type && params.tax_identifier_type !== 'cpf') return invalidTaxIdentifierType;
   else return ok;
 };
 
@@ -43,3 +45,17 @@ var decline = {
   }
 };
 
+/**
+ * invalid tax identifier error. returned when CPF is invalid or type is not 'cpf'
+ */
+
+var invalidTaxIdentifierType = {
+  error: {
+    code: 'validation',
+    message: DECLINED_TAX_IDENTIFIER_TYPE,
+    fields: ['tax_identifier_type'],
+    details: [
+      { field: 'tax_identifier_type', messages: [DECLINED_TAX_IDENTIFIER_TYPE] }
+    ]
+  }
+};
