f010cb04af6495dd90cc29d2d3eba1464e4e03dc
Emmanuel Feygelson <efeygelson@recurly.com>
2021-07-06 16:40:46 -0400

    Add test for cuit tax identifiers

--- test/server/fixtures/token/index.js
+++ test/server/fixtures/token/index.js
@@ -5,13 +5,13 @@
 var DECLINE_CARD = '4000000000000002';
 var INVALID_JSON = '5454545454545454';
 var DECLINED_MESSAGE = 'Your card was declined. In order to resolve the issue, you will need to contact your bank.';
-var DECLINED_TAX_IDENTIFIER_TYPE = 'Tax identifier type must be one of the following: ["cpf"]';
+var DECLINED_TAX_IDENTIFIER_TYPE = 'Tax identifier type must be one of the following: ["cpf", "cuit"]';
 
 module.exports = function token () {
   var params = this.method === 'GET' ? this.query : this.request.body;
   if (params.number === DECLINE_CARD) return decline;
   if (params.number === INVALID_JSON) return invalidJson;
-  if (params.tax_identifier_type && params.tax_identifier_type !== 'cpf') return invalidTaxIdentifierType;
+  if (params.tax_identifier_type && !~['cpf', 'cuit'].indexOf(params.tax_identifier_type)) return invalidTaxIdentifierType;
   else return ok;
 };
 
