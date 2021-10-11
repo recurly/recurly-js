d3c2afdd2bbeeb9ed4d4998c2ed799b769634bea
Alexander <aleyva@recurly.com>
2021-10-11 09:42:51 -0300

    Chase 3DS Support.

---
+++ test/unit/risk/three-d-secure/strategy/orbital.test.js
@@ -0,0 +1,59 @@
+import assert from 'assert';
+import { applyFixtures } from '../../../support/fixtures';
+import { initRecurly, testBed } from '../../../support/helpers';
+import OrbitalStrategy from '../../../../../lib/recurly/risk/three-d-secure/strategy/orbital';
+import actionToken from '../../../../server/fixtures/tokens/action-token-orbital.json';
+
+describe('OrbitalStrategy', function () {
+  this.ctx.fixture = 'threeDSecure';
+
+  applyFixtures();
+
+  beforeEach(function (done) {
+    const recurly = this.recurly = initRecurly();
+    const risk = recurly.Risk();
+    const threeDSecure = this.threeDSecure = risk.ThreeDSecure({ actionTokenId: 'action-token-test' });
+    this.target = testBed().querySelector('#three-d-secure-container');
+
+    this.sandbox = sinon.createSandbox();
+    this.sandbox.spy(recurly, 'Frame');
+
+    this.strategy = new OrbitalStrategy({ threeDSecure, actionToken });
+    this.strategy.whenReady(() => done());
+  });
+
+  afterEach(function () {
+    const { sandbox, strategy } = this;
+    strategy.remove();
+    sandbox.restore();
+  });
+
+  describe('attach', function () {
+    it('creates a frame using the actionToken params', function () {
+      const { strategy, target, recurly } = this;
+      strategy.attach(target);
+      assert(recurly.Frame.calledOnce);
+      assert(recurly.Frame.calledWithMatch({
+        path: '/three_d_secure/start',
+        payload: {
+          redirect_url: 'test-orbital-acs-url',
+          pa_req: 'test-orbital-pa-req',
+          md: 'test-orbital-md',
+          creq: 'test-orbital-creq',
+          three_d_secure_action_token_id: 'action-token-orbital'
+        },
+        defaultEventName: 'orbital-3ds-challenge'
+      }));
+    });
+  });
+
+  describe('remove', function () {
+    it('destroys any existing frame', function () {
+      const { strategy, target, sandbox } = this;
+      strategy.attach(target);
+      sandbox.spy(strategy.frame, 'destroy');
+      strategy.remove();
+      assert(strategy.frame.destroy.calledOnce);
+    });
+  });
+});
