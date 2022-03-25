c6945d81d99582a073307573babf4452dbaf461b
Stephanie <smagdic@recurly.com>
2022-03-25 09:48:24 -0600

    Add console logs to test build

--- test/e2e/display.test.js
+++ test/e2e/display.test.js
@@ -77,16 +77,22 @@ maybeDescribe('Display', () => {
 
 function assertDiffThresholdMet (diff) {
   let threshold;
+  console.log("threshold", threshold)
   if (environmentIs(DEVICES.ANDROID)) {
     threshold = 0.5;
   } else if (environmentIs(DEVICES.IOS)) {
     threshold = 0.25;
   } else if (environmentIs(BROWSERS.EDGE)) {
+    console.log("I am here in the else block for the edge environment!")
+    // should be getting here, which means that the threshold should be truthy.
     threshold = 0.06;
   } else if (environmentIs(BROWSERS.FIREFOX)) {
    threshold = 0.1;
   }
+  // which means it should be going to this first if statement.
   if (threshold) assert(diff <= threshold, `${diff} is above the threshold of ${threshold}`);
+  // going into this else block -> (error message: 0.06 !== 0)
+  // But Edge has a threshold, so should it not go into the if block above and make that assertion instead. *
   else assert.strictEqual(diff, 0);
 }
 
