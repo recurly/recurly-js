59d46cdf49b40252caf8ca68e16ba4b2d7c4050e
Adam Bachman <abachman@recurly.com>
2022-11-15 15:19:29 -0500

    add 4 additional partitions to IE11 test suite to attempt to avoid errors on Browserstack

--- test/unit/support/init.js
+++ test/unit/support/init.js
@@ -6,13 +6,19 @@
  * BROWSER=ChromeHeadless SUITE_PARTITION=A make test-unit
  */
 
+const PARTITIONS = ['A', 'B', 'C', 'D', 'E', 'F']; // add letters here and .travis.yml for IE11 as necessary
+
+function skipPartition (testIdx) {
+  return SUITE_PARTITION !== PARTITIONS[testIdx % PARTITIONS.length];
+}
+
 function overrideItForPartitions () {
   const origIt = it;
-  const partition = mod => SUITE_PARTITION === 'A' ? mod : !mod;
   let count = 0;
 
+  // eslint-disable-next-line no-global-assign
   it = function (...args) {
-    if (typeof args[args.length-1] === 'function' && partition(count++ % 2)) {
+    if (typeof args[args.length - 1] === 'function' && skipPartition(count++)) {
       return origIt.skip(...args);
     }
     return origIt(...args);
