640471c06af5ba2a08d35033a2aa986657d5937a
Douglas Lise <dlise@recurly.com>
2023-02-17 15:55:43 -0300

    Fixed addon_price for tiered addons

--- test/unit/pricing/subscription/tiered-pricing-calculator.test.js
+++ test/unit/pricing/subscription/tiered-pricing-calculator.test.js
@@ -1,9 +1,12 @@
 import assert from 'assert';
-import { getTieredPricingTotal } from '../../../../lib/recurly/pricing/subscription/tiered-pricing-calculator';
+import {
+  getTieredPricingTotal,
+  getTieredPricingUnitAmount
+} from '../../../../lib/recurly/pricing/subscription/tiered-pricing-calculator';
 import { initRecurly } from '../../support/helpers';
 import TIERED_PLAN from '../../../server/fixtures/plans/tiered-plan.json';
 
-describe('Recurly.Pricing.Subscription.TieredPricingCalendar', function () {
+describe('Recurly.Pricing.Subscription.TieredPricingCalculator', function () {
   beforeEach(function () {
     this.recurly = initRecurly();
     this.pricing = this.recurly.Pricing.Subscription();
@@ -29,6 +32,30 @@ describe('Recurly.Pricing.Subscription.TieredPricingCalendar', function () {
       assert.strictEqual(getTieredPricingTotal(volumeAddon, 2, 'USD'), 4.00);
       assert.strictEqual(getTieredPricingTotal(volumeAddon, 6, 'USD'), 24.00);
       done();
+    });
+
+    describe('unit pricing for current unit amount', () => {
+      it('should return the current tier price for tiered pricing', function (done) {
+        const tieredAddon = TIERED_PLAN.addons.filter((addon) => { return addon.code === 'tiered'; })[0];
+
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 1, 'USD'), 2.00);
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 2, 'USD'), 2.00);
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 5, 'USD'), 2.00);
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 6, 'USD'), 4.00);
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 50, 'USD'), 4.00);
+        done();
+      });
+
+      it('should return the current tier price for stairstep pricing', function (done) {
+        const tieredAddon = TIERED_PLAN.addons.filter((addon) => { return addon.code === 'stairstep'; })[0];
+
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 1, 'USD'), 2.00);
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 2, 'USD'), 2.00);
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 5, 'USD'), 2.00);
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 6, 'USD'), 4.00);
+        assert.strictEqual(getTieredPricingUnitAmount(tieredAddon, 50, 'USD'), 4.00);
+        done();
+      });
     });
   });
 });
